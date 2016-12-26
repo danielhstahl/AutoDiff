42c3f7a3a51b0a81656c6f376e637c7932c5f745
Daniel Stahl <danstahl1138@gmail.com>
2016-12-26 09:17:51 -0600

    added complex test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "AutoDiff.h"
-
+#include <complex>
 
 TEST_CASE("Test polynomial", "[Functional]"){
     AutoDiff<double> myAutoDiff(5.0, 1.0);
@@ -24,4 +24,18 @@ TEST_CASE("Test exponential", "[Functional]"){
 
     REQUIRE(myResult.getStandard()==Approx(exp(5.0)));
     REQUIRE(myResult.getDual()==Approx(exp(5.0)));
+}
+
+TEST_CASE("Test complex", "[Functional]"){
+    AutoDiff<std::complex<double> > myAutoDiff(std::complex<double>(0.0, 4.0), std::complex<double>(0.0, 1.0));
+    auto myExp=[&](const auto& myX){
+        return exp(myX);
+    };
+    auto myResult=myExp(myAutoDiff);
+
+
+    REQUIRE(myResult.getStandard().real()==Approx(cos(4.0)));
+    REQUIRE(myResult.getStandard().imag()==Approx(sin(4.0)));
+    REQUIRE(myResult.getDual().real()==Approx(-sin(4.0)));
+    REQUIRE(myResult.getDual().imag()==Approx(cos(4.0)));
 }
